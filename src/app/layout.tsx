14b4b1b243afff2e08ddd26c8377bbf38234ac0a
darosss <darosss12@proton.me>
2025-06-19 01:22:48 +0200

    style(tailwind): add tailwindCSS css/options etc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import styles from "./layout.module.scss";
+import "@/styles/globals.css";
 import "@/styles/globals.scss";
 import "react-toastify/dist/ReactToastify.css";
 import "react-tooltip/dist/react-tooltip.css";
@@ -20,8 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className={styles.contentBackground}></div>
+      <body
+        className={`${inter.className} text-gray-100 min-h-screen relative`}
+      >
+        <div className="bg-gradient-to-br from-blue-900 via-black to-gray-900 absolute top-0 right-0 bottom-0 left-0"></div>
         <div>
           <LayoutContent>{children}</LayoutContent>
         </div>

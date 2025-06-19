0962ab5984b407d333b585cd78ef43e1eb6c0484
darosss <darosss12@proton.me>
2025-06-19 12:38:56 +0200

    refactor(components/auth): remove logout-button

--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -1,5 +1,4 @@
 export { LoginForm } from "./login-form";
 export { RegisterForm } from "./register-form";
-export { LogoutButton } from "./logout-button";
 
 export { AuthContextProvider, useAuthContext } from "./auth-context";

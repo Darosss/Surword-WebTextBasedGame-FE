94f7dd12b8d683847a8d97c65c5f8c60f045576f
darosss <darosss12@proton.me>
2025-06-19 12:38:15 +0200

    refactor(components/login-form): replace module.css with tailwind

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import styles from "./login-form.module.scss";
 import Cookies from "js-cookie";
 import { FC, FormEvent } from "react";
 import { COOKIE_TOKEN_NAME, fetchBackendApi } from "@/api/fetch";
 import { useRouter } from "next/navigation";
-import { Button } from "@/components/common";
 import { useAuthContext } from "@/components/auth";
+import { Button } from "../components/ui/button";
 
 type LoginResponse = {
   email: string;
@@ -51,21 +50,37 @@ export const LoginForm: FC = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className={styles.form}>
-      <div>
-        <label>Email</label>
-        {/* TODO: make it email */}
-        <input type="text" name="email" required />
+    <div className="container mx-auto px-4 py-8 flex flex-col items-center gap-1">
+      <h1 className="text-3xl font-bold mb-2 text-blue-300">Login</h1>
+      <div className="w-full p-3 max-w-md bg-gray-800/50 rounded-lg shadow-xl">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-1 pb-4 pt-4">
+          <div className="text-gray-300 mb-2 flex justify-between">
+            <label className="grow max-w-1/2 text-center">Email</label>
+            <input
+              className="max-w-1/2 grow border rounded-sm"
+              type="email"
+              name="email"
+              required
+            />
+          </div>
+          <div className="text-gray-300 mb-2 flex justify-between">
+            <label className="grow max-w-1/2 text-center">Password</label>
+            <input
+              className="max-w-1/2 grow border rounded-sm"
+              type="password"
+              name="password"
+              required
+            />
+          </div>
+          <Button
+            type="submit"
+            variant="ghost"
+            className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-4 rounded-lg transition-colors"
+          >
+            Login
+          </Button>
+        </form>
       </div>
-      <div>
-        <label>Password</label>
-        <input type="password" name="password" required />
-      </div>
-      <div>
-        <Button type="submit" defaultButtonType="primary">
-          Login
-        </Button>
-      </div>
-    </form>
+    </div>
   );
 };

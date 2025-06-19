a0e435f2feef55a62dee47f260fa4b78e30aeedd
darosss <darosss12@proton.me>
2025-06-19 12:33:49 +0200

    refactor(components/user-details): replace module.css with tailwind

--- src/components/user/user-details.tsx
+++ src/components/user/user-details.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import styles from "./user-details.module.scss";
 import { useAuthContext } from "@/components/auth";
 import { HeroDetails } from "./hero-details";
 
@@ -16,12 +15,12 @@ export const UserDetails: FC = () => {
   if (!username) return null;
 
   return (
-    <div className={styles.userDetailsWrapper}>
+    <div className="flex justify-around">
       <div>
-        Username: <span>{username}</span>
+        Username: <span className="text-primary">{username}</span>
       </div>
       <div>
-        Gold: <span>{gold}</span>
+        Gold: <span className="text-warning">{gold}</span>
       </div>
 
       {heroDetails ? <HeroDetails details={heroDetails} /> : null}

d5cd1b12b3a91995ddd9757cbfe9221f18be93ac
darosss <darosss12@proton.me>
2025-06-19 12:38:24 +0200

    refactor(components/register-form): replace module.css with tailwind

--- src/components/auth/register-form.tsx
+++ src/components/auth/register-form.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import styles from "./register-form.module.scss";
 import { FC, FormEvent } from "react";
 import { fetchBackendApi } from "@/api/fetch";
 import { useRouter } from "next/navigation";
-import { Button } from "@/components/common";
+
 import { User } from "@/api/types";
+import { Button } from "../components/ui/button";
 
 type LoginResponse = User;
 
@@ -45,25 +45,46 @@ export const RegisterForm: FC = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className={styles.form}>
-      <div>
-        <label>Email</label>
-        {/* TODO: make it email */}
-        <input type="text" name="email" required />
+    <div className="container mx-auto px-4 py-8 flex flex-col items-center gap-1">
+      <h1 className="text-3xl font-bold mb-2 text-blue-300">Register</h1>
+      <div className="w-full max-w-md p-8 bg-gray-800/50 rounded-lg shadow-xl">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-1 pb-4 pt-4">
+          <div className="text-gray-300 mb-2 flex justify-between">
+            <label className="grow max-w-1/2 text-center">Email</label>
+            <input
+              className="max-w-1/2 grow border rounded-sm"
+              type="email"
+              name="email"
+              required
+            />
+          </div>
+          <div className="text-gray-300 mb-2 flex justify-between">
+            <label className="grow max-w-1/2 text-center">Username</label>
+            <input
+              className="max-w-1/2 grow border rounded-sm"
+              type="text"
+              name="username"
+              required
+            />
+          </div>
+          <div className="text-gray-300 mb-2 flex justify-between">
+            <label className="grow max-w-1/2 text-center">Password</label>
+            <input
+              className="max-w-1/2 grow border rounded-sm"
+              type="password"
+              name="password"
+              required
+            />
+          </div>
+          <Button
+            type="submit"
+            variant="ghost"
+            className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-4 rounded-lg transition-colors"
+          >
+            Register
+          </Button>
+        </form>
       </div>
-      <div>
-        <label>Username</label>
-        <input type="text" name="username" required />
-      </div>
-      <div>
-        <label>Password</label>
-        <input type="password" name="password" required />
-      </div>
-      <div>
-        <Button type="submit" defaultButtonType="primary">
-          Register
-        </Button>
-      </div>
-    </form>
+    </div>
   );
 };

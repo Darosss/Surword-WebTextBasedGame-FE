32d6dfefcf50ab63aa0903d849d052bf3a4cfae1
darosss <darosss12@proton.me>
2025-06-25 23:51:33 +0200

    styles(pages/dungeons): add tailwind styles

--- src/app/dungeons/page.tsx
+++ src/app/dungeons/page.tsx
@@ -1,11 +1,16 @@
 import { Dungeons } from "@/components/dungeons";
-import styles from "./dungeons.module.scss";
 
 export default async function DungeonsPage() {
   return (
     <main>
-      <h1>Dungeons</h1>
-      <Dungeons />
+      <div className="flex flex-col flex-grow gap-4 min-h-[calc(100vh-10rem)]">
+        <div className="w-full p-4 rounded-lg bg-gray-800/60 border border-gray-700 h-full flex flex-col">
+          <h2 className="text-2xl font-bold text-yellow-300 border-b border-gray-600 pb-3 mb-4">
+            Dungeons
+          </h2>
+          <Dungeons />
+        </div>
+      </div>
     </main>
   );
 }

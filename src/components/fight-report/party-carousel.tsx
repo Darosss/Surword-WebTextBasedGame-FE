db81d0dc463d8ee08dc4f183f091571e07df56ff
darosss <darosss12@proton.me>
2025-08-09 12:06:17 +0200

    refactor(components/PartyCarousel): replace participants props to be Map of ids + data - add keys based on id - health (for animation purposes) - connected to those changes - smaller style fixes

--- src/components/fight-report/party-carousel.tsx
+++ src/components/fight-report/party-carousel.tsx
@@ -1,4 +1,4 @@
-import { FightReportType } from "@/api/types";
+import { Character, NpcEnemy } from "@/api/types";
 import { cn } from "@/lib/utils";
 import {
   Carousel,
@@ -11,7 +11,7 @@ import ParticipantCard from "./participant.card";
 import { FC } from "react";
 
 type PartyCarouselProps = {
-  participants: FightReportType["characters"] | FightReportType["enemies"];
+  participants: Map<string, Character> | Map<string, NpcEnemy>;
   partyName: string;
   icon: React.ElementType;
   iconColor: string;
@@ -41,37 +41,36 @@ export const PartyCarousel: FC<PartyCarouselProps> = ({
       >
         <IconComponent className={cn("h-5 w-5 mr-2", iconColor)} /> {partyName}
       </h2>
-      {participants.length > 0 ? (
-        participants.length > 1 ? (
-          <Carousel
-            opts={{ align: "start", loop: participants.length > 3 }}
-            className="w-full max-w-full"
-          >
-            <CarouselContent className="-ml-2 sm:-ml-3">
-              {participants.map((p, index) => (
-                <CarouselItem
-                  key={JSON.stringify(p.id) + index}
-                  className="pl-2 sm:pl-3 basis-1/2 md:basis-1/3 lg:basis-1/2 xl:basis-1/3"
-                >
-                  <ParticipantCard participant={p} team={team} />
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            {participants.length >
-              (typeof window !== "undefined" && window.innerWidth < 1024
-                ? 2
-                : 3) && ( // Conditional arrows based on items and viewport
-              <>
-                <CarouselPrevious className="absolute left-[-8px] sm:left-[-12px] top-1/2 -translate-y-1/2 h-7 w-7 bg-gray-700/90 hover:bg-gray-600 border-gray-600 text-gray-300 z-10" />
-                <CarouselNext className="absolute right-[-8px] sm:right-[-12px] top-1/2 -translate-y-1/2 h-7 w-7 bg-gray-700/90 hover:bg-gray-600 border-gray-600 text-gray-300 z-10" />
-              </>
-            )}
-          </Carousel>
-        ) : (
-          <div className="px-2 sm:px-3">
-            <ParticipantCard participant={participants[0]} team={team} />
-          </div>
-        )
+      {participants.size > 0 ? (
+        <Carousel
+          opts={{ align: "start", loop: participants.size > 3 }}
+          className="w-full max-w-full"
+        >
+          <CarouselContent className="-ml-2 sm:-ml-3">
+            {[...participants.values()].map((p) => (
+              <CarouselItem
+                key={`${p.id}-${p.health}`}
+                className={cn(
+                  `pl-2 sm:pl-3`,
+                  participants.size > 1
+                    ? "basis-1/2 md:basis-1/3 lg:basis-1/2 xl:basis-1/3"
+                    : "flex-grow"
+                )}
+              >
+                <ParticipantCard participant={p} team={team} />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          {participants.size >
+            (typeof window !== "undefined" && window.innerWidth < 1024
+              ? 2
+              : 3) && ( // Conditional arrows based on items and viewport
+            <>
+              <CarouselPrevious className="absolute left-[-8px] sm:left-[-12px] top-1/2 -translate-y-1/2 h-7 w-7 bg-gray-700/90 hover:bg-gray-600 border-gray-600 text-gray-300 z-10" />
+              <CarouselNext className="absolute right-[-8px] sm:right-[-12px] top-1/2 -translate-y-1/2 h-7 w-7 bg-gray-700/90 hover:bg-gray-600 border-gray-600 text-gray-300 z-10" />
+            </>
+          )}
+        </Carousel>
       ) : (
         <p className="text-gray-500 text-sm px-2 sm:px-3">
           No participants in this group.
